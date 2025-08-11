164bd3d218d15e44a80014002713b4c41347efd3
Steve <gaulmaru@gmail.com>
2025-08-11 13:48:58 +0900

    fix: remove unused React import & apply App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import RacingDraw from "./RacingDraw";
+import "./App.css"; // 스타일 적용
 
 export default function App() {
-  return <RacingDraw />;
+  return (
+    <div id="root">
+      <RacingDraw />
+    </div>
+  );
 }
